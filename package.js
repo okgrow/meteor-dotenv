c51690c6884e561ad7ec3f621f376edff49d9956
Paul Dowman <paul@pauldowman.com>
2014-09-13 11:03:34 -0400

    Convert from meteorite to unipackage

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
-  summary: "Set environment variables defined in a .env file"
+  name:    "pauldowman:dotenv",
+  version: "1.0.0",
+  summary: "Set environment variables defined in a .env file",
+  git:     "https://github.com/okgrow/meteor-dotenv/"
 });
 
 Npm.depends({dotenv: "0.4.0"});
 
-Package.on_use(function (api) {
-  api.add_files('lib/dotenv.js', 'server');
+Package.onUse(function (api) {
+  api.addFiles('lib/dotenv.js', 'server');
 });
